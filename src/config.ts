6067de524aad60ab19472528b2d226cd45aeceb4
gangulisabyasachi <ganguli.sabyasachi2705@gmail.com>
2026-02-13 18:36:14 +0530

    commit

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Prithwish Ganguli",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "Prithwish Ganguli | Blog",
+  title: "Prithwish Ganguli",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,

1a177a95fc1a2d48280dfc0b15e41a748215ea1a
gangulisabyasachi <ganguli.sabyasachi2705@gmail.com>
2026-03-04 19:06:43 +0530

    correct

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { SITE } from "@/config";
 
-export const BLOG_PATH = "src/data/blog/march2026";
+export const BLOG_PATH = "src/data/blog/";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),

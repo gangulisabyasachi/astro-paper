861d3af885edbe4e4d635b0a8518e82b4752922c
gangulisabyasachi <ganguli.sabyasachi2705@gmail.com>
2026-04-01 01:20:59 +0530

    font

--- src/utils/loadGoogleFont.ts
+++ src/utils/loadGoogleFont.ts
@@ -36,14 +36,14 @@ async function loadGoogleFonts(
 > {
   const fontsConfig = [
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Libre Baskerville",
+      font: "Libre+Baskerville",
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
-      font: "IBM+Plex+Mono",
+      name: "Libre Baskerville",
+      font: "Libre+Baskerville",
       weight: 700,
       style: "bold",
     },

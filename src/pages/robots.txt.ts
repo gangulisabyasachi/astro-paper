0027b7efd22e2d7cc0267a1b45cbcaff59a575ad
gangulisabyasachi <ganguli.sabyasachi2705@gmail.com>
2026-04-04 13:06:07 +0530

    robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,6 +4,14 @@ const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
+# Explicitly allow AdSense crawler
+User-agent: Mediapartners-Google
+Allow: /
+
+# Optional: allow Google Image bot
+User-agent: Googlebot-Image
+Allow: /
+
 Sitemap: ${sitemapURL.href}
 `;
 
